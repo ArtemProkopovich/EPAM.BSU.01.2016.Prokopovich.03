7ba9fba638b18e45d4e25ed9ffdb074560472876
ArtemProkopovich <artemprokopovich96@gmail.com>
2016-03-17 03:22:02 +0300

    Добавлен класс Polynom  и Unit-тесты к нему.

---
+++ Polynom_TestLibrary/Polynom_TestClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+
+namespace Polynom_TestLibrary
+{
+    [TestFixture]
+    public class Polynom_TestClass
+    {
+        [Test]
+        [TestCase(new double[] {0, 1, 2, 3},Result= "3,00*x^3+2,00*x^2+1,00*x^1+0,00")]
+        [TestCase(new double[] { 0, -1, 2, -3 }, Result = "-3,00*x^3+2,00*x^2-1,00*x^1+0,00")]
+        [TestCase(new double[0], Result = "")]
+        [TestCase(null,ExpectedException = typeof(ArgumentNullException))]
+        public string Polynom_ToString_Test(double[] array)
+        {
+            return new Polynom(array).ToString();
+        }
+
+        [Test]
+        [TestCase(new double[] {0, 1, 2, 3}, new double[] {5, 6, 7, 8}, Result = "11,00*x^3+9,00*x^2+7,00*x^1+5,00")]
+        [TestCase(new double[] {0, 1, -2, 3}, new double[] {-5, -6, -7, 8}, Result = "11,00*x^3-9,00*x^2-5,00*x^1-5,00")]
+        [TestCase(new double[] {0, 1, -2, 3}, new double[] {-5, 6}, Result = "3,00*x^3-2,00*x^2+7,00*x^1-5,00")]
+        [TestCase(new double[] {0, 1}, new double[] {-5, 6, 7, -8}, Result = "-8,00*x^3+7,00*x^2+7,00*x^1-5,00")]
+        [TestCase(new double[] { 0, 1, 2 }, new double[0], Result = "2,00*x^2+1,00*x^1+0,00")]
+        [TestCase(new double[] { 0, 1, -2, 3 }, new double[] { 5, -1, 2, -3 }, Result = "5,00")]
+        public string Polynom_Add_Test(double[] arr1, double[] arr2)
+        {
+            return (new Polynom(arr1) + new Polynom(arr2)).ToString();
+        }
+
+        [Test]
+        [TestCase(new double[] { 0, 1, 2, 3 }, new double[] { 5, 6, 7, 8 }, Result = "-5,00*x^3-5,00*x^2-5,00*x^1-5,00")]
+        [TestCase(new double[] { 0, 1, -2, 3 }, new double[] { -5, -6, -7, 8 }, Result = "-5,00*x^3+5,00*x^2+7,00*x^1+5,00")]
+        [TestCase(new double[] { 0, 1, -2, 3 }, new double[] { -5, 6 }, Result = "3,00*x^3-2,00*x^2-5,00*x^1+5,00")]
+        [TestCase(new double[] { 0, 1 }, new double[] { -5, 6, 7, -8 }, Result = "8,00*x^3-7,00*x^2-5,00*x^1+5,00")]
+        [TestCase(new double[] { 0, 1, 2 }, new double[0], Result = "2,00*x^2+1,00*x^1+0,00")]
+        [TestCase(new double[] { 0, 1, -2, 3 }, new double[] { 5, 1, -2, 3 }, Result = "-5,00")]
+        public string Polynom_Substruct_Test(double[] arr1, double[] arr2)
+        {
+            return (new Polynom(arr1) - new Polynom(arr2)).ToString();
+        }
+
+        [Test]
+        [TestCase(new double[] {0, 1}, new double[] {5, 6}, Result = "6,00*x^2+5,00*x^1+0,00")]
+        [TestCase(new double[] {-1}, new double[] {5, 6, -9}, Result = "9,00*x^2-6,00*x^1-5,00")]
+        [TestCase(new double[] {-1, 2}, new double[] {5, -6}, Result = "-12,00*x^2+16,00*x^1-5,00")]
+        [TestCase(new double[0], new double[] {5, -6}, Result = "0,00")]
+        [TestCase(new double[] {5, 6}, new double[] {0}, Result = "0,00")]
+        [TestCase(new double[] {5, 6, 7}, new double[] {-1, 2}, Result = "14,00*x^3+5,00*x^2+4,00*x^1-5,00")]
+        [TestCase(new double[] {5, 6, 7}, new double[] {0}, Result = "0,00")]
+        public string Polynom_Multiply_Test(double[] arr1, double[] arr2)
+        {
+            return (new Polynom(arr1)*new Polynom(arr2)).ToString();
+        }
+
+        [Test]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1 }, Result = true)]
+        [TestCase(new double[] { -1 }, new double[] { 5, 6, -9 }, Result = false)]
+        [TestCase(new double[0], new double[0], Result = true)]
+        [TestCase(new double[0], null, Result = false)]
+        public bool Polynom_Equals_Test(double[] arr1, double[] arr2)
+        {
+            if (arr2 == null)
+                return new Polynom(arr1).Equals(null);
+            return new Polynom(arr1).Equals(new Polynom(arr2));
+        }
+
+        [Test]
+        public void Polynom_Equals_Ref_Test()
+        {
+            Polynom p = new Polynom(new double[] {0, 1, 2});
+            object p1 = p;
+            Assert.IsTrue(p.Equals(p1));
+            Assert.IsFalse(p.Equals("123"));
+        }
+
+        [Test]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1 }, Result = false)]
+        [TestCase(new double[] { -1 }, new double[] { 5, 6, -9 }, Result = false)]
+        public bool Polynom_GetHashCode_Test(double[] arr1, double[] arr2)
+        {
+            Polynom p = new Polynom(arr1);
+            Polynom p1 = p;
+            Assert.AreEqual(p1.GetHashCode(), p.GetHashCode());
+            return p.GetHashCode() == new Polynom(arr2).GetHashCode();
+            
+        }
+    }
+}
---
+++ Task1/Polynom.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Polynom
+    {
+        private readonly double[] coefficients;
+
+        public int Power => coefficients.Length;
+
+        public double this[int index] => coefficients[index];
+
+        public Polynom()
+        {
+            coefficients = new double[0];
+        }
+
+        public Polynom(double[] coef)
+        {
+            if (coef == null)
+                throw new ArgumentNullException(nameof(coef));
+            int i = coef.Length - 1;
+            while (i > 0 && coef[i] == 0)
+                i--;
+            coefficients = new double[i + 1];
+            for (int j = 0; j <= i; j++)
+            {
+                coefficients[j] = coef[j];
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (ReferenceEquals(obj, this))
+                return true;
+            return this.Equals(obj as Polynom);          
+        }
+
+        public bool Equals(Polynom p)
+        {
+            if (p?.Power != this.Power)
+                return false;
+            for (int i = 0; i < this.Power; i++)
+                if (p[i] != this[i])
+                    return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return coefficients.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if (Power == 0)
+                return "";
+            StringBuilder result = new StringBuilder();
+            for (int i = Power - 1; i > 0; i--)
+            {
+                if (coefficients[i] >= 0 && i != Power - 1)
+                    result.Append("+");
+                result.AppendFormat($"{coefficients[i]:F2}*x^{i}");
+            }
+            if (coefficients[0] >= 0 && Power > 1)
+                result.Append("+");
+            result.Append($"{coefficients[0]:F2}");
+            return result.ToString();
+        }
+
+        public static Polynom Add(Polynom pol1, Polynom pol2)
+        {        
+            int maxPower = Math.Max(pol1.Power, pol2.Power);
+            double[] resultCoef = new double[maxPower];
+            for (int i = 0; i < pol1.Power; i++)
+                resultCoef[i] += pol1[i];
+            for (int i = 0; i < pol2.Power; i++)
+                resultCoef[i] += pol2[i];
+            return new Polynom(resultCoef);
+        }
+
+        public static Polynom operator +(Polynom pol1, Polynom pol2)
+        {
+            return Add(pol1, pol2);
+        }
+
+        public Polynom Add(Polynom polynom)
+        {
+            return Add(this, polynom);
+        }
+
+        public static Polynom operator -(Polynom pol1, Polynom pol2)
+        {
+            return Substruct(pol1, pol2);
+        }
+
+        public static Polynom Substruct(Polynom pol1, Polynom pol2)
+        {
+            int maxPower = Math.Max(pol1.Power, pol2.Power);
+            double[] resultCoef = new double[maxPower];
+            for (int i = 0; i < pol1.Power; i++)
+                resultCoef[i] += pol1[i];
+            for (int i = 0; i < pol2.Power; i++)
+                resultCoef[i] -= pol2[i];
+            return new Polynom(resultCoef);
+        }
+
+        public Polynom Substruct(Polynom polynom)
+        {
+            return Substruct(this, polynom);
+        }
+
+        public static Polynom operator *(Polynom pol1, Polynom pol2)
+        {
+            return Multiply(pol1, pol2);
+        }
+
+        public static Polynom Multiply(Polynom pol1, Polynom pol2)
+        {
+            int maxPower = pol1.Power + pol2.Power - 1;
+            double[] resultCoef = new double[maxPower];
+            for (int i = 0; i < pol1.Power; i++)
+                for (int j = 0; j < pol2.Power; j++)
+                {
+                    resultCoef[i + j] += pol1[i]*pol2[j];
+                }
+            return new Polynom(resultCoef);
+        }
+
+        public Polynom Multiply(Polynom polynom)
+        {
+            return Multiply(this, polynom);
+        }
+    }
+}
